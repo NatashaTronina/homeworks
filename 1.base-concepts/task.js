8cb3593a031d2f951362bc7ab3277211552573e3
Natasha Tronina <tronina.2004@yandex.ru>
2025-06-21 23:49:47 +0500

    Решение первой и второй задачи

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,24 +3,29 @@ function solveEquation(a, b, c) {
   let arr = [];
   let discriminant = b ** 2 - 4 * a * c;
   if (discriminant < 0){
-    arr.push()
+    arr.push();
   }
   else if (discriminant === 0){
     let x = -b / (2 * a);
     arr.push(x)
   }
   else{
-    let x = (-b + Math.sqrt(discriminant) )/(2*a)
-    arr.push(x)
-    let y = (-b - Math.sqrt(discriminant) )/(2*a)
-    arr.push(y)
+    let x = (-b + Math.sqrt(discriminant) )/(2*a);
+    arr.push(x);
+    let y = (-b - Math.sqrt(discriminant) )/(2*a);
+    arr.push(y);
   }
   return arr;
 }
-let result = solveEquation(1, 5, 4);
-console.log("Наши корни:")
-console.log(result);
+
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let P = (percent / 100) / 12;
+  let S = amount - contribution;
+  let n = countMonths
+
+  let monthlyPayment = S * (P + (P / (((1 + P) ** n) - 1)));
+
+  let result = monthlyPayment * n;
+  return Number(result.toFixed(2))
 }
