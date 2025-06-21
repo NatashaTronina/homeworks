62b336f1294349dd8d45728043075e6bc55c3c93
Natasha Tronina <tronina.2004@yandex.ru>
2025-06-21 23:18:17 +0500

    Решение первой задачи

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,9 +1,25 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discriminant = b ** 2 - 4 * a * c;
+  if (discriminant < 0){
+    arr.push()
+  }
+  else if (discriminant === 0){
+    let x = -b / (2 * a);
+    arr.push(x)
+  }
+  else{
+    let x = (-b + Math.sqrt(discriminant) )/(2*a)
+    arr.push(x)
+    let y = (-b - Math.sqrt(discriminant) )/(2*a)
+    arr.push(y)
+  }
   return arr;
 }
+let result = solveEquation(1, 5, 4);
+console.log("Наши корни:")
+console.log(result);
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
