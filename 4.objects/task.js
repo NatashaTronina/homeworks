0922a7b5ec47ccce4d68228a2266351c79240bba
Natasha Tronina <tronina.2004@yandex.ru>
2025-06-27 02:37:17 +0500

    Все тесты проходят, кроме последнего

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,38 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if(marks.length < 1){
+        return 0
+    }else if (this.hasOwnProperty('excluded')){
+        return
+    }else{
+       this.marks.push(...marks);
+    }
 }
 
+
 Student.prototype.getAverage = function () {
-  
+    if(this.marks.length < 1){
+        return 0
+    }else{
+        let sum = this.marks.reduce((acc, mark) => acc + mark, 0)
+        return sum / this.marks.length;
+    }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject
+  delete this.marks
+  this.excluded = reason;
 }
+
+
