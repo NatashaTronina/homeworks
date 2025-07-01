2bbc3f78f36d7b18aff2be0be169cc1e8511d715
Natasha Tronina <tronina.2004@yandex.ru>
2025-07-01 21:18:31 +0500

    Решила первую задачу

---
+++ 5.classes/task.js
@@ -0,0 +1,69 @@
+class PrintEditionItem {
+    constructor(name, releaseDate, pagesCount, state = 100, type = null) {
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.pagesCount = pagesCount;
+        this._state = state; // Используем _state для хранения значения
+        this.type = type;
+    }
+
+    fix() {
+        this.state *= 1.5;
+        if (this.state > 100) {
+            this.state = 100; // Ограничиваем значение 100
+        }
+    }
+
+    set state(newState) {
+        if (newState < 0) {
+            this._state = 0;
+        } else if (newState > 100) {
+            this._state = 100;
+        } else {
+            this._state = newState;
+        }
+    }
+
+    get state() {
+        return this._state;
+    }
+}
+
+class Magazine extends PrintEditionItem {
+    constructor(name, releaseDate, pagesCount, state = 100, type = "magazine") {
+        super(name, releaseDate, pagesCount, state, type); // Передаем type в super()
+        this.type = type; // Устанавливаем type в конструкторе
+    }
+}
+
+class Book extends PrintEditionItem {
+    constructor(author, name, releaseDate, pagesCount, state = 100, type) {
+        super(name, releaseDate, pagesCount, state, type);
+        this.author = author;
+        this.type = "book";
+    }
+}
+
+class NovelBook extends Book {
+    constructor(name, releaseDate, pagesCount, author, state = 100, type) {
+        super(name, releaseDate, pagesCount, author, state, type);
+        this.type = "novel";
+    }
+}
+
+class FantasticBook extends Book {
+    constructor(name, releaseDate, pagesCount, author, state = 100, type) {
+        super(name, releaseDate, pagesCount, author, state, type);
+        this.type = "fantastic";
+    }
+}
+
+class DetectiveBook extends Book {
+    constructor(name, releaseDate, pagesCount, author, state = 100, type) {
+        super(name, releaseDate, pagesCount, author, state, type);
+        this.type = "detective";
+    }
+}
+
+
+
