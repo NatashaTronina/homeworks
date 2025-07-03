455f5bc616169ade57b5a62a96fe9f24c0de6118
Natasha Tronina <tronina.2004@yandex.ru>
2025-07-04 00:10:21 +0500

    Решила 1 задачу

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,16 @@
-﻿
+﻿function parseCount(num){
+    let result = Number.parseFloat(num)
+    if(isNaN(result)){
+        const error = new Error("Невалидное значение");
+        throw error;
+    }
+    return result;
+}
+
+function validateCount(number){
+    try{
+        return parseCount(number)
+    }catch(error){
+        return error
+    }
+}
